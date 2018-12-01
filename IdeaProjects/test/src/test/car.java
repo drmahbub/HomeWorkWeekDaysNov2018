429476aab009bfd6f66b35288647049559cb181a
drmahbub <drmahbub29@gmail.com>
2018-11-30 19:58:02 -0800

    Test2

--- IdeaProjects/test/src/test/car.java
+++ IdeaProjects/test/src/test/car.java
@@ -5,8 +5,10 @@ public class car {
     public static void main(String[] args) {
 
         String abc ="Hello";
+        int mno = 2;
                 String def = abc.replace("He","NO");
-                System.out.print(def);
+                System.out.println(def);
+                System.out.print(mno);
 
 
     }
