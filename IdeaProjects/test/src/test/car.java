1784ba9c0f4ffdbaaf5ccf7df83398d47e1b8a8e
drmahbub <drmahbub29@gmail.com>
2018-11-30 19:30:37 -0800

    test

---
+++ IdeaProjects/test/src/test/car.java
@@ -0,0 +1,15 @@
+package test;
+
+public class car {
+
+    public static void main(String[] args) {
+
+        String abc ="Hello";
+                String def = abc.replace("He","NO");
+                System.out.print(def);
+
+
+    }
+
+
+}
